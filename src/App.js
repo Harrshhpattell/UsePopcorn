453f420b2b824a7a193831feae05858d89967ccc
Patel Harsh <102842153+Harrshhpattell@users.noreply.github.com>
2023-08-29 15:24:50 +0530

    final

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ export default function App() {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
